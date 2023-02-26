3fae7b75a6e0c9be4d5f81117d0f79e02a3f1496
Ivo Nunes <inunes1904@gmail.com>
2023-02-26 16:41:51 +0000

    MenuItems SingleMenuItem

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('menu-items', views.MenuItemsView.as_view()),
-    path('categories', views.CategoriesView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('category', views.CategoriesView.as_view()),
+    path('groups/manager/users', views.ManagersView.as_view()),
+    path('groups/manager/users/<int:pk>', views.SingleManagerView.as_view()),
+    path('groups/delivery-crew/users', views.DeliveryCrewsView.as_view()),
+    path('groups/delivery-crew/users/<int:pk>', views.DeliveryCrewsView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
+    path('orders', views.OrdersView.as_view()),
+    path('orders/<int:pk>', views.SingleOrderView.as_view()),    
+    path('ratings', views.RatingsView.as_view()),
     path('secret', views.secret),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth', obtain_auth_token),
  
 ]
